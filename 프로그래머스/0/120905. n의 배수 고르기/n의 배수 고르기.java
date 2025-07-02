5f786bd726f50dd606338989f7fc07bb19303869
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-07-03 08:56:55 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int count = 0;
+        
+        for(int i=0; i<numlist.length; i++){
+            if(numlist[i] % n == 0){
+                count++;
+            }
+        }
+        int answer[] = new int[count];
+        int a = 0;
+        
+        for(int j=0; j<numlist.length; j++){
+            if(numlist[j] % n == 0){
+                answer[a] = numlist[j];
+                a++;
+            }
+        }
+        
+        return answer;
+    }
+}
