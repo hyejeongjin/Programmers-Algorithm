fefedf90701b7efc38260b9537fcbe71f65343df
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-07-22 17:01:47 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 1.25 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length-5];
+        int a = 0;
+        Arrays.sort(num_list);
+        for(int i=5; i<num_list.length; i++){
+            
+            answer[a++] = num_list[i];
+        }
+        
+       
+        return answer;
+    }
+}
