387cdcd30452dc86875c79c430be8a245e9d87fd
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-07-24 01:43:47 +0900

    [level 0] Title: ad 제거하기, Time: 3.20 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -2,14 +2,7 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strArr) {
-        List<String> list = new ArrayList<>();
-        
-        for (String s : strArr) {
-            if (!s.contains("ad")) {
-                list.add(s);
-            }
-        }
-        
-        return list.toArray(new String[0]);
+       
+        return Arrays.stream(strArr).filter(str -> !str.contains("ad")).toArray(String[]::new);
     }
 }
