750846a0ec0b2ed4047417f603ad798f1c29922e
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-07-29 10:43:50 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.21 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = new String[my_string.split(" ").length];
+        
+        answer = my_string.split(" ");
+        return answer;
+    }
+}
