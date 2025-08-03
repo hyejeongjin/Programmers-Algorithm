8111f495350fa2e485a2495fab5470eb394c618e
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-08-04 00:30:22 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -1,26 +1,37 @@
+import java.lang.Math;
+
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         int odd = 0;
         int even = 0;
         
-        // odd
+//         // odd
+//         for(int i=0; i<num_list.length; i++){
+//             odd += num_list[i];
+//             i++;
+//         }
+//         // even
+//         for(int i=1; i<num_list.length; i++){
+//             even += num_list[i];
+//             i++;
+//         }
+        
+//         if(odd > even){
+//             answer = odd;
+//         }else{
+//             answer = even;
+//         }
+        
         for(int i=0; i<num_list.length; i++){
-            odd += num_list[i];
-            i++;
+            if(i % 2 == 0){
+                odd += num_list[i];
+            }else{
+                even += num_list[i];
+            }
         }
-        // even
-        for(int i=1; i<num_list.length; i++){
-            even += num_list[i];
-            i++;
-        }
-        
-        if(odd > even){
-            answer = odd;
-        }else{
-            answer = even;
-        }
-        
+        answer = Math.max(odd, even);
+
         return answer;
     }
 }
