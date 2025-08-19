ccb2548f5bec24cb696ac5e1955ca4ba3f566003
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-08-20 01:36:05 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a % 2 != 0 && b % 2 != 0){
+            answer = (int)(Math.pow(a, 2) + Math.pow(b, 2));
+        }else if(a % 2 != 0 || b % 2 != 0){
+            answer = 2*(a + b);
+        }else{
+            answer = Math.abs(a - b);
+        }
+        return answer;
+    }
+}
