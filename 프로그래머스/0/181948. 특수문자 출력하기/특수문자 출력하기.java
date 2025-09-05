043a31f83c7bf35c574589c559561a1938451602
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-09-06 00:14:08 +0900

    [level 0] Title: 특수문자 출력하기, Time: 143.18 ms, Memory: 62.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // \와 " 앞에 \룰 붙여줘야함.
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
