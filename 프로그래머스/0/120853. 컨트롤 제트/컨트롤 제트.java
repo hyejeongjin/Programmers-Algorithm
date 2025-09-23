59a7d33a6e447d3f2cc402fcca0863af5f27ba90
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-09-24 01:21:12 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.21 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] list = s.split(" ");
+        
+        for(int i=0; i<list.length; i++){
+            if(list[i].contains("Z")){
+                answer -= Integer.parseInt(list[i-1]);
+            }else{
+                answer += Integer.parseInt(list[i]);
+            }
+        }
+        
+        
+        return answer;
+    }
+}
