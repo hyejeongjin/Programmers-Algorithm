92ef7309769763a26c715682d12a55f55c37fcca
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-10-01 01:02:36 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int result = 0;
+        
+        result = Integer.parseInt(n_str);
+        answer = String.valueOf(result);
+        
+        return answer;
+    }
+}
