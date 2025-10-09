bcee5be652a2b52a2c81c061f43efa6b1333c709
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-10-10 00:41:24 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.44 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -4,9 +4,16 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         
-        int[] list = Arrays.stream(s.split(" "))
-                   .mapToInt(Integer::parseInt)
-                   .toArray();
+        // int[] list = Arrays.stream(s.split(" "))
+        //            .mapToInt(Integer::parseInt)
+        //            .toArray();
+        
+        String[] arr = s.split(" ");
+        int[] list = new int[arr.length];
+        
+        for(int i=0; i<arr.length; i++){
+            list[i] = Integer.parseInt(arr[i]);
+        }
         
         Arrays.sort(list);
         answer = list[0] + " " + list[list.length - 1];
