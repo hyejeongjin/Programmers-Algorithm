e3fdb8c8317dc213e8e2bcea48431d18d28a37f4
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-11-20 00:09:20 +0900

    [level 0] Title: 배열 비교하기, Time: 0.04 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        
+        if(arr1.length == arr2.length){
+            int num1 = 0;
+            int num2 = 0;
+            for(int i=0; i<arr1.length; i++){
+                num1 += arr1[i];
+                num2 += arr2[i];
+            }
+            if(num1 > num2){
+                answer = 1;
+            }
+            if(num1 < num2){
+                answer = -1;
+            }
+        }
+        if(arr1.length > arr2.length){
+            answer = 1;
+        }
+        if(arr1.length < arr2.length){
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}
