cbddd68e8e40f64641668dfe50c68375a69f57be
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-12-01 00:17:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -5,23 +5,30 @@ class Solution {
         int answer = 0;
         String number = s;
         
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("zero", "0");
-        map.put("one", "1");
-        map.put("two", "2");
-        map.put("three", "3");
-        map.put("four", "4");
-        map.put("five", "5");
-        map.put("six", "6");
-        map.put("seven", "7");
-        map.put("eight", "8");
-        map.put("nine", "9");
+//         HashMap<String, String> map = new HashMap<String, String>();
+//         map.put("zero", "0");
+//         map.put("one", "1");
+//         map.put("two", "2");
+//         map.put("three", "3");
+//         map.put("four", "4");
+//         map.put("five", "5");
+//         map.put("six", "6");
+//         map.put("seven", "7");
+//         map.put("eight", "8");
+//         map.put("nine", "9");
         
-        for(Map.Entry<String, String> entry : map.entrySet()){
-            number = number.replace(entry.getKey(), entry.getValue());
+//         for(Map.Entry<String, String> entry : map.entrySet()){
+//             number = number.replace(entry.getKey(), entry.getValue());
+//         }
+        
+        String[] numlist = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i=0; i<numlist.length; i++){
+            if(s.contains(numlist[i])){
+                number = number.replace(numlist[i], Integer.toString(i));
+            }
         }
         answer = Integer.parseInt(number);
-        
         return answer;
     }
 }
