4e5d44eafeed6ffb8257749bc285097ad8b08cc2
pandasticday <121605597+hyejeongjin@users.noreply.github.com>
2025-12-07 20:49:01 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int gcd = getGCD(a, b);
+        b = b / gcd;
+        
+        while(b % 2 == 0){
+            b /= 2;
+        }
+        
+        while(b % 5 == 0){
+            b /= 5;
+        }
+        
+        if(b == 1){
+            answer = 1;
+        }
+        if(b != 1){
+            answer = 2;
+        }
+
+        return answer;
+    }
+    
+    private int getGCD(int n, int m){
+        while(m != 0){
+            int temp = n % m;
+            n = m;
+            m = temp;
+        }
+        return n;
+    }
+    
+}
